de10b996b8eb9a3adda8624470ec0b29daaa5cd2
yoon-dh <acttoze1@gmail.com>
2023-06-20 00:32:02 +0900

    coordinate refactoring

--- window_size/coordinates/main.js
+++ window_size/coordinates/main.js
@@ -3,17 +3,29 @@ const vertical = document.querySelector(".vertical");
 const horizontal = document.querySelector(".horizontal");
 const tag = document.querySelector(".tag");
 
-document.addEventListener("mousemove", (e) => {
-  let x = e.clientX;
-  let y = e.clientY;
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
+  document.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
+    //   target.style.left = `${x}px`;
+    //   target.style.top = `${y}px`;
+    target.style.transform = `translate(${x - targetHalfWidth}px,${
+      y - targetHalfHeight
+    }px)`;
 
-  tag.innerHTML = `${x}px, ${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    //   vertical.style.left = `${x}px`;
+    //   horizontal.style.top = `${y}px`;
+
+    tag.innerHTML = `${x}px, ${y}px`;
+    //   tag.style.left = `${x}px`;
+    //   tag.style.top = `${y}px`;
+    tag.style.transform = `translate(${x}px,${y}px)`;
+  });
 });
